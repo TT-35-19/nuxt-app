2ea60d63802c80f955352ee99b734ef4d2dcd3eb
TaiyoshiTakahiro <popo0129tt35@gmail.com>
2022-07-05 00:05:16 +0900

    add : axios

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,19 @@
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+  data() {
+    return {
+      movies: [],
+    }
+  },
+  methods: {
+    async getMovies() {
+      const data = this.axios.get(
+        'https://api.themoviedb.org/3/movie/550?api_key=f1eeb07b7c050d06a34c73f15c027788'
+      )
+    }
+  }
 }
 </script>
